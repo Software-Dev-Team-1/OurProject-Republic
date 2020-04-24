4af1b8951763ffd267ba0a616383f245ad75ee0d
bllemay <brian.lemay@colorado.edu>
2020-04-24 17:03:39 -0600

    semi functional login portal

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//dbPassword = 'mongodb+srv://brle3863@colorado.edu'+ encodeURIComponent('COLbuffs1') + '@OurRepublic.mongodb.net/test?retryWrites=true';
+dbPassword ='mongodb+srv://BrianL:$COLbuffs1@ourrepublic-0xcae.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- resources/php/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn_string= "host= localhost port=3000 dbname=test user=postgres password=$9DENbro84$";
-$dbconn= pg_connect($conn_string);
-
-if(!$dbconn){
- die('Could not Connect pql:' .pg_error());
-}
-?>
