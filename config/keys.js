48600582e81d1581774c07966d007a014cdebbdc
bllemay <brian.lemay@colorado.edu>
2020-04-26 13:00:36 -0600

    update

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-//dbPassword = 'mongodb+srv://brle3863@colorado.edu'+ encodeURIComponent('COLbuffs1') + '@OurRepublic.mongodb.net/test?retryWrites=true';
+
 dbPassword ='mongodb+srv://BrianL:$COLbuffs1@ourrepublic-0xcae.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
